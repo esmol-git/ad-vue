9fecd1dd7add5119b449708af2b855d87b86023c
Evgeny <esmol@gmai.com>
2019-12-23 16:22:55 +0300

    ad-vue

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>star</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="'Text link'"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app dark color="primary">
+      <v-toolbar-side-icon @click="drawer=!drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Ad application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>bug_report</v-icon>Link 1
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
