c62861be08e84da4958c46fa2c76ed90c1b7ad9e
Evgeny <esmol@gmai.com>
2019-12-23 18:51:06 +0300

    login

---
+++ src/components/Ads/Ad.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-layiut row>
+      <v-flex xs12>
+        <h1>Ad</h1>
+      </v-flex>
+    </v-layiut>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
